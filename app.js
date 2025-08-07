6e51d105e5d0290a1ab6d128390e2bb12f3b2c70
Aaron Castillo <castillo.aaron9802@gmail.com>
2025-08-06 20:21:31 -0600

    Agregar funcion para registro de nuevos amigos y mandar alert en caso de que campo esté vacío

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaAmigos = [];
+
+//Función llamada a partir del botón de añadir para agregar elemento a lista de amigos
+function agregarAmigo(){
+    let nuevoAmigo = document.getElementById("amigo");
+    if(nuevoAmigo.value === "" || nuevoAmigo.value === null){
+        alert("Por favor, inserte un nombre.");
+    }else{
+        listaAmigos.push(nuevoAmigo.value);
+        nuevoAmigo.value = "";
+    }
+}
